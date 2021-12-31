f8c469ad26f27a6a3b45dec5bdadb9f8ba5a11c3
Pufikas <dominykas.guzys@gmail.com>
2021-12-31 16:20:09 +0200

    4

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ window.addEventListener('load', ()=> {
   let lat;
 
   if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position =>{console.log(position)})
+    navigator.geolocation.getCurrentPosition(position =>{ 
+      long = position.coords.longitude
+      lat = position.coords.latitude  
+
+      const proxy = 'https://cors-anywhere.herokuapp.com/'
+      const api = `6676c4c24ec54d80090a4fa61664238a`
+      
+      fetch(api)
+      .then(data => {
+        return data.json()
+      })
+      .then(data => {
+        console.log(data)
+      })
+
+
+      console.log(position)})
   }else{
     h1.textContent = "Cannot find the location"
   }
